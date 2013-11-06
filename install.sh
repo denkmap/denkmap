0f8966661e60ad67f701b56e2faed90302d64479
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-11-06 11:04:17 +0100

    Add grunt for linting

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ unzip sencha-touch-2.3.0.gpl.zip
 
 # Download LeafletMap extension
 wget -P ux https://raw.github.com/tschortsch/Ext.ux.LeafletMap/f541eb2d69faf1ba3b40b3964208b1e8f05c9652/ux/LeafletMap.js 
+
+# Install dependencies
+npm install -g grunt-cli
+npm install
