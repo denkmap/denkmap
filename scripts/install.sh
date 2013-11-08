e5c7c2f07420466b1ce27a1986b7dea3c0a15812
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-11-08 07:43:13 +0100

    Added download of denkmal.geojson

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,6 +12,9 @@ fi
 # Download LeafletMap extension
 wget -P $DIR/../ux https://raw.github.com/tschortsch/Ext.ux.LeafletMap/f541eb2d69faf1ba3b40b3964208b1e8f05c9652/ux/LeafletMap.js 
 
+# Download Denkmal GeoJSON
+wget -P $DIR/../resources/remote https://dl.dropboxusercontent.com/u/138759/denkmal.geojson
+
 # Install dependencies
 npm install -g grunt-cli
 npm install
