5a18cb534aa16afc2e8f228126db099c1d451402
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-11-11 15:38:55 +0100

    Downloading the Sencha Touch SDK is not necessary

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,11 +3,11 @@ set -e
 
 DIR=`dirname $0`
 
-if [[ -z $TRAVIS ]] ; then
-    # Download Sencha Touch 2.3
-    wget http://cdn.sencha.com/touch/sencha-touch-2.3.0-gpl.zip
-    unzip sencha-touch-2.3.0.gpl.zip
-fi
+# if [[ -z $TRAVIS ]] ; then
+#     # Download Sencha Touch 2.3
+#     wget http://cdn.sencha.com/touch/sencha-touch-2.3.0-gpl.zip
+#     unzip sencha-touch-2.3.0.gpl.zip
+# fi
 
 # Download LeafletMap extension
 wget -P $DIR/../ux https://raw.github.com/tschortsch/Ext.ux.LeafletMap/f541eb2d69faf1ba3b40b3964208b1e8f05c9652/ux/LeafletMap.js 
@@ -20,5 +20,5 @@ wget -P $DIR/../resources/remote https://raw.github.com/calvinmetcalf/leaflet-aj
 
 
 # Install dependencies
-npm install -g grunt-cli
+sudo npm install -g grunt-cli
 npm install
