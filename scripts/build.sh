1abb37b213ecc89ae4969af3587b7717727f4858
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-02-19 15:44:51 +0100

    Replace build nr in production build

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,7 +16,8 @@ $DIR/install.sh
 grunt lint --verbose
 
 if [[ $TRAVIS_BUILD_NUMBER ]] ; then
-    sed -i "s/{BUILD_NR}/$TRAVIS_BUILD_NUMBER/" $DIR/../app/util/Config.js     
+    sed -i "s/{BUILD_NR}/$TRAVIS_BUILD_NUMBER/" $DIR/../app/util/Config.js
+    sed -i "s/{BUILD_NR}/$TRAVIS_BUILD_NUMBER/" $DIR/../build/production/Denkmap/app.js
 fi
 
 exit 0
